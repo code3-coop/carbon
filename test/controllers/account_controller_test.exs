3126967353c19cae683793bed4bd1dd1f7bb1e67
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-03 12:04:13 -0400

    Add small test

--- test/controllers/account_controller_test.exs
+++ test/controllers/account_controller_test.exs
@@ -5,6 +5,25 @@ defmodule Carbon.AccountControllerTest do
   @valid_attrs %{}
   @invalid_attrs %{}
 
+  setup do
+    joe = Repo.insert! %Carbon.User{handle: "joe", email_hash: "joe"}
+    {:ok, [user: joe]}
+  end
+
+  test "creates resources and redirects when successful", %{conn: conn, user: joe} do
+    conn = put_token_req_cookie conn, joe.id
+    conn = post conn, account_path(conn, :create), account: %{name: "test account 1"}
+
+    account = Repo.get_by!(Account, %{name: "test account 1"})
+    assert account.owner_id == joe.id
+    assert redirected_to(conn) == account_path(conn, :show, account.id)
+  end
+
+  defp put_token_req_cookie(conn, user_id) do
+    token_name = Carbon.SessionController.carbon_token_name
+    put_resp_cookie(conn, token_name, Phoenix.Token.sign(Carbon.Endpoint, token_name, user_id))
+  end
+
   # test "lists all entries on index", %{conn: conn} do
     # conn = put_token_cookie(conn)
     # conn = get conn, account_path(conn, :index)
@@ -15,17 +34,6 @@ defmodule Carbon.AccountControllerTest do
     # conn = get conn, account_path(conn, :new)
     # assert html_response(conn, 200) =~ "New account"
   # end
-
-  test "creates resources and redirects when successful", %{conn: conn} do
-    joe = Repo.insert! %Carbon.User{handle: "joe", email_hash: "joe"}
-
-    conn = put_token_req_cookie conn, joe.id
-    conn = post conn, account_path(conn, :create), account: %{name: "test account 1"}
-
-    account = Repo.get_by!(Account, %{name: "test account 1"})
-    assert account.owner_id == joe.id
-    assert redirected_to(conn) == account_path(conn, :show, account.id)
-  end
 
   # test "creates resource and redirects when data is valid", %{conn: conn} do
     # conn = post conn, account_path(conn, :create), account: @valid_attrs
@@ -72,8 +80,4 @@ defmodule Carbon.AccountControllerTest do
     # refute Repo.get(Account, account.id)
   # end
 
-  defp put_token_req_cookie(conn, user_id) do
-    token_name = Carbon.SessionController.carbon_token_name
-    put_resp_cookie(conn, token_name, Phoenix.Token.sign(Carbon.Endpoint, token_name, user_id))
-  end
 end
