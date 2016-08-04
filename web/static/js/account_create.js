6ade64e993ff7e03ba392a189d459b39198aaa3b
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-03 21:40:21 -0400

    Some styling...

--- web/static/js/account_create.js
+++ web/static/js/account_create.js
@@ -1,6 +1,1 @@
-import $ from 'jquery'
-import 'semantic-ui-css/components/form'
-import 'semantic-ui-css/components/dropdown'
-import 'semantic-ui-css/components/transition'
-
-$('.ui.selection.dropdown').dropdown()
+$('select.dropdown').dropdown()
