a38befabec4c98bc37fb5f36fa97080c1bf37bda
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-08-04 16:57:35 -0400

    add tag on event, and render it

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,7 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Carbon.{Repo, AccountStatus, Account, Contact, Address, Event, User, AccountTag}
+alias Carbon.{Repo, AccountStatus, Account, Contact, Address, Event, User, AccountTag, EventTag}
 
 
 joe = Repo.insert! %User{ handle: "joe", full_name: "Joe", title: "Awesome", email_hash: (:crypto.hash(:sha, "joe@erlang.com") |> Base.encode16 |> String.downcase) }
@@ -19,7 +19,8 @@ lead_status = Repo.insert!(%AccountStatus{key: "lead"})
 customer_status = Repo.insert!(%AccountStatus{key: "customer"})
 former_customer_status = Repo.insert!(%AccountStatus{key: "former_customer"})
 
-slow_payer = Repo.insert!(%AccountTag{description: "slow_payer", color: "red"})
+slow_payer = Repo.insert!(%AccountTag{description: "Slow payer", color: "red"})
+usless_meeting = Repo.insert!(%EventTag{description: "Useless meeting", color: "red"})
 
 account_a = Repo.insert!(%Account{name: "Account A", owner: joe, status: customer_status, tags: [slow_payer]})
 account_b = Repo.insert!(%Account{name: "Account B", owner: joe, status: customer_status})
@@ -33,7 +34,7 @@ billing_address_b = Repo.insert! %Address{street_address: "1 Billing Street", lo
 shipping_address_a = Repo.insert! %Address{street_address: "1 Shipping Street", locality: "City A", region: "RA", country_name: "CA", account: account_a}
 shipping_address_b = Repo.insert! %Address{street_address: "1 Shipping Street", locality: "City B", region: "RB", country_name: "CB", account: account_b}
 
-Repo.insert! %Event{description: "1st meeting", date: Ecto.DateTime.from_erl(:calendar.local_time), account: account_a, user: joe}
+Repo.insert! %Event{description: "1st meeting", date: Ecto.DateTime.from_erl(:calendar.local_time), account: account_a, user: joe, tags: [usless_meeting]}
 Repo.insert! %Event{description: "2nd meeting", date: Ecto.DateTime.from_erl(:calendar.local_time), account: account_a, user: joe}
 
 Repo.insert! %Event{description: "1st meeting", date: Ecto.DateTime.from_erl(:calendar.local_time), account: account_b, user: joe}
