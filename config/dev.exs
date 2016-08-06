0ca13ccc5e07e813a3b26439350a47de4f64914e
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-08-06 08:01:28 -0400

    remove watcher for brunch

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,7 @@ config :carbon, Carbon.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-               cd: Path.expand("../", __DIR__)]]
+  watchers: []
 
 # Watch static and templates for browser reloading.
 config :carbon, Carbon.Endpoint,
