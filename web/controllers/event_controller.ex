b03dea1ac84553151b2d4a30c13ea86581f4a12c
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-07 20:36:32 -0400

    Small changes in eventcontroller

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -1,13 +1,13 @@
 defmodule Carbon.EventController do
   use Carbon.Web, :controller
 
-  alias Carbon.{ Event, Reminder }
+  alias Carbon.{ Account, Event, Reminder }
 
-  def index(conn, %{"account_id" => id}) do
+  def index(conn, %{"account_id" => account_id}) do
     current_user = conn.assigns[:current_user]
     
     events_query = from e in Event,
-      where: e.account_id == ^id and e.active == true,
+      where: e.account_id == ^account_id and e.active == true,
       left_join: t in assoc(e, :tags),
       left_join: u in assoc(e, :user),
       left_join: er in Reminder, on:
@@ -18,19 +18,21 @@ defmodule Carbon.EventController do
       order_by: [desc: e.date],
       preload: [ tags: t, reminders: er, user: u ]
 
-    account_query = from a in Carbon.Account,
-      select: a.name,
-      where: a.id == ^id
-    render(conn, "index.html", events: Repo.all(events_query),
-                               account_name: Repo.one(account_query),
-                               account_id: id)
+    conn
+    |> assign(:events, Repo.all(events_query))
+    |> assign(:account, Repo.get(Account, account_id))
+    |> render("index.html")
   end
 
   def new(conn, %{"account_id" => account_id}) do
-    changeset = Event.changeset(%Event{})
     conn
-    |> assign(:changeset, changeset)
-    |> render("new.html", account_id: account_id)
+    |> assign(:changeset, Event.changeset(%Event{}))
+    |> assign(:account_id, account_id)
+    |> render("new.html")
+  end
+
+  def create(conn, %{"account_id" => account_id}, event_params) do
+    conn
   end
 
 end
