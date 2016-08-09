ddb8b0a5d02e2755313fd4a9fa74f9456c0c4ad4
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-09 09:51:53 -0400

    FTS multiple documents and intersect results

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -21,37 +21,48 @@ defmodule Carbon.SearchController do
   "
 
   def search(conn, %{ "search" => %{ "query" => user_query }}) do
-    case Ecto.Adapters.SQL.query(Repo, @search_query, [ user_query ]) do
-      {:ok, %{:num_rows => 0}} ->
-        conn
-        |> select_similar_term(user_query)
-        |> assign(:matches_by_account_id, %{})
-        |> assign(:accounts, [])
-        |> assign(:query, user_query)
-        |> render(Carbon.AccountView, "index.html")
-      {:ok, %{:rows => rows}} ->
-        conn
-        |> assign(:similar, nil)
-        |> assign(:matches_by_account_id, build_matches_by_account_dict(rows))
-        |> assign(:query, user_query)
-        |> select_matching_accounts(rows)
-        |> render(Carbon.AccountView, "index.html")
-      {:error, _e} ->
-        conn
-        |> put_flash(:error, "Error while executing search")
-        |> render(Carbon.AccountView, "index.html")
+    all_rows = user_query
+    |> String.split(~r{\s+}, trim: true)
+    |> Enum.map(&Task.async(Ecto.Adapters.SQL, :query, [Repo, @search_query, [ &1 ]]))
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&extract_rows/1)
+
+    ids = all_rows
+    |> Enum.map(&build_set_of_ids/1)
+    |> Enum.reduce(&MapSet.intersection/2)
+    |> MapSet.to_list
+
+    if Enum.empty?(ids) do
+      conn
+      |> assign(:similar, select_similar_term(user_query))
+      |> assign(:matches_by_account_id, %{})
+      |> assign(:accounts, [])
+      |> assign(:query, user_query)
+      |> render(Carbon.AccountView, "index.html")
+    else
+      conn
+      |> assign(:similar, nil)
+      |> assign(:matches_by_account_id, build_and_merge_matches_dicts(all_rows))
+      |> assign(:accounts, select_matching_accounts(ids))
+      |> assign(:query, user_query)
+      |> render(Carbon.AccountView, "index.html")
     end
   end
 
-  defp select_similar_term(conn, user_query) do
-    case Ecto.Adapters.SQL.query(Repo, @similar_term_query, [ user_query ]) do
-      {:ok, %{:num_rows => 0}} ->
-        assign(conn, :similar, nil)
-      {:ok, %{:rows => rows}} ->
-        assign(conn, :similar, hd rows)
-      {:error, _e} ->
-        put_flash(conn, :error, "Error while executing search")
-    end
+  defp extract_rows({:error, _}), do: []
+  defp extract_rows({:ok, %{:num_rows => 0}}), do: []
+  defp extract_rows({:ok, %{:rows => rows}}), do: rows
+
+  defp build_set_of_ids(rows) do
+    rows
+    |> Enum.map(&Enum.at(&1, 0))
+    |> MapSet.new
+  end
+
+  defp build_and_merge_matches_dicts(all_rows) do
+    all_rows
+    |> Enum.map(&build_matches_by_account_dict/1)
+    |> Enum.reduce(&(Map.merge(&1, &2, fn (_id, m1, m2) -> Enum.concat(m1, m2) end)))
   end
 
   defp build_matches_by_account_dict(rows) do
@@ -60,13 +71,20 @@ defmodule Carbon.SearchController do
     end
   end
 
-  defp select_matching_accounts(conn, rows) do
-    account_ids = rows |> MapSet.new(&Enum.at(&1, 0)) |> MapSet.to_list
+  defp select_similar_term(user_query) do
+    case Ecto.Adapters.SQL.query(Repo, @similar_term_query, [ user_query ]) do
+      {:error, _e} -> nil
+      {:ok, %{:num_rows => 0}} -> nil
+      {:ok, %{:rows => rows}} -> hd rows
+    end
+  end
+
+  defp select_matching_accounts(account_ids) do
     query = from a in Account,
       where: a.id in ^account_ids,
       left_join: c in assoc(a, :contacts),
       left_join: b in assoc(a, :billing_address),
       preload: [contacts: c, billing_address: b]
-    assign(conn, :accounts, Repo.all(query))
+    Repo.all(query)
   end
 end
