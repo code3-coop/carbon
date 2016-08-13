ff844c3d31a8e148027a9f5b071b76d4d409e5f7
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-13 13:21:02 -0400

    Add duration parser

---
+++ lib/carbon/duration.ex
@@ -0,0 +1,34 @@
+defmodule Carbon.Duration do
+
+  @work_hours_per_day 7
+
+  @doc """
+  Parses a duration description. Returns the corresponding number of minutes as
+  as integer. Duration descriptions are given in numeric value followed by a
+  unit descriptor such as "m", "h", or "d".
+
+  ## Examples
+  
+      iex> Carbon.Duration.parse_minutes("30m")
+      30
+
+      iex> Carbon.Duration.parse_minutes("1.5h")
+      90
+
+      iex> Carbon.Duration.parse_minutes("1d")
+      420
+  """
+  def parse_minutes(duration_string) do
+    if String.match?(duration_string, ~r/^\d+(?:\.\d+)?[mhd]$/) do
+      [ value_string, unit ] = String.split(duration_string, ~r/(?=[mhd]$)/)
+      { value, _ } = Float.parse value_string
+      value |> in_minutes(unit) |> round
+    else
+      0
+    end
+  end
+
+  defp in_minutes(value, "m"), do: value
+  defp in_minutes(value, "h"), do: value * 60
+  defp in_minutes(value, "d"), do: value * @work_hours_per_day * 60
+end
