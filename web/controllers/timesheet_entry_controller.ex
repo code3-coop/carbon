de68d424c17e367e781f5a617449ec5c0a27f235
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-08-18 20:34:58 -0400

    add edit/update for timesheet_entry

--- web/controllers/timesheet_entry_controller.ex
+++ web/controllers/timesheet_entry_controller.ex
@@ -12,14 +12,6 @@ defmodule Carbon.TimesheetEntryController do
     |> render("new.html")
   end
 
-  def edit(conn, %{"timesheet_id" => timesheet_id, "id" => entry_id}) do
-    timesheet_entry = Repo.get!(TimesheetEntry, entry_id) |> Repo.preload([:tags, :project, :account])
-    conn
-    |> assign(:changeset, TimesheetEntry.update_changeset(timesheet_entry))
-    |> assign(:timesheet_entry, timesheet_entry)
-    |> render("edit.html")
-  end
-
   def create(conn, %{"timesheet_id" => timesheet_id, "timesheet_entry" => timesheet_entry_params}) do
     tags = get_tags_from(TimesheetEntryTag, timesheet_entry_params)
     timesheet_id = String.to_integer(timesheet_id)
@@ -41,6 +33,49 @@ defmodule Carbon.TimesheetEntryController do
         |> assign(:changeset, changeset)
         |> assign(:timesheet_id, timesheet_id)
         |> render("new.html")
+    end
+  end
+
+  def edit(conn, %{"timesheet_id" => timesheet_id, "id" => entry_id}) do
+    timesheet_entry = Repo.get!(TimesheetEntry, entry_id) |> Repo.preload([:tags, :project, :account])
+    conn
+    |> assign(:changeset, TimesheetEntry.update_changeset(timesheet_entry))
+    |> assign(:timesheet_entry, timesheet_entry)
+    |> render("edit.html")
+  end
+
+  def update(conn, %{"timesheet_id" => timesheet_id, "id" => entry_id, "timesheet_entry" => timesheet_entry_params}) do
+    tags = get_tags_from(TimesheetEntryTag, timesheet_entry_params)
+    timesheet_id = String.to_integer(timesheet_id)
+
+    timesheet_entry_query = from te in TimesheetEntry,
+      where: te.id == ^entry_id,
+      left_join: t in assoc(te, :tags),
+      left_join: p in assoc(te, :project),
+      left_join: a in assoc(te, :account),
+      left_join: ts in assoc(te, :timesheet),
+      preload: [ tags: t, project: p, account: a, timesheet: ts]
+
+
+    timesheet_entry = Repo.one(timesheet_entry_query)
+    ctx = get_context_from(timesheet_entry_params)
+    # timesheet_entry = %TimesheetEntry{
+    #   timesheet_id: timesheet_id,
+    #   project: ctx.project,
+    #   account: ctx.account,
+    #   duration_in_minutes: Carbon.Duration.parse_minutes(timesheet_entry_params["duration_in_minutes"])
+    # }
+    changeset = TimesheetEntry.update_changeset(timesheet_entry, timesheet_entry_params, tags)
+    case Repo.update(changeset) do
+      {:ok, timesheet_entry} ->
+        conn
+        |> put_flash(:info, "Timesheet entry updated successfully.")
+        |> redirect(to: timesheet_path(conn, :edit, timesheet_entry.timesheet_id))
+      {:error, changeset} ->
+        conn
+        |> assign(:changeset, changeset)
+        |> assign(:timesheet_id, timesheet_id)
+        |> render("edit.html")
     end
   end
 
