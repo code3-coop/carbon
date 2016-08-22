d8f1c78f0d6ce9c3a3302dff6adff99774dd1b2e
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-22 15:54:55 -0400

    Execute query every 5 minutes

--- lib/carbon/email_notification_sender.ex
+++ lib/carbon/email_notification_sender.ex
@@ -1,7 +1,13 @@
 defmodule Carbon.EmailNotificationSender do
   use GenServer
 
+  import Ecto.Query, only: [from: 2]
+
   @five_minutes_in_millis 5 * 60 * 1000
+
+  @query from r in Carbon.Reminder,
+    join: u in assoc(r, :user),
+    where: r.active and not r.seen and not r.sent_by_email and u.send_email_reminders and ago(1, "hour") <= r.date and r.date < from_now(1, "minute")
 
   def start_link do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
@@ -12,7 +18,9 @@ defmodule Carbon.EmailNotificationSender do
     { :ok, %{} }
   end
 
-  def handle_cast(:check_and_send, state) do
+  def handle_info(:check_and_send, state) do
+    Carbon.Repo.all @query
+
     check_later
     { :noreply, state }
   end
