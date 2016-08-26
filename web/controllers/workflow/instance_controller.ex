8186c4a422cfdbeaeb570c535af2a8d53257f3e0
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-08-26 17:38:20 -0400

    Better errror handling

--- web/controllers/workflow/instance_controller.ex
+++ web/controllers/workflow/instance_controller.ex
@@ -7,23 +7,22 @@ defmodule Carbon.Workflow.InstanceController do
   def index(conn, _params) do
     fetch_workflows = Task.async Repo, :all, [(from w in Workflow, preload: :states)]
 
-    query = from i in Instance, preload: [ :workflow, :state, [ values: :field ] ]
-    instances = Repo.all(query)
+    instances = Repo.all from i in Instance, preload: [ :workflow, :state, [ values: :field ] ]
     
     references = instances
     |> Enum.flat_map(&(&1.values))
     |> Enum.reduce(%{}, &accumulate_ids/2)
 
-    [ { _pid1, { :ok, accounts } }, { _pid2, { :ok, users } } ] = Task.yield_many [
+    { accounts, users } = extract_references Task.yield_many [
       create_fetch_task(Account, Map.get(references, :account_ids)),
       create_fetch_task(User, Map.get(references, :user_ids))
     ]
 
     conn
-    |> assign(:workflows, Task.await(fetch_workflows))
     |> assign(:instances, instances)
     |> assign(:accounts, accounts)
     |> assign(:users, users)
+    |> assign(:workflows, Task.await(fetch_workflows))
     |> render("index.html")
   end
 
@@ -41,4 +40,9 @@ defmodule Carbon.Workflow.InstanceController do
   defp create_fetch_task(module, ids) do
     Task.async(Repo, :all, [(from i in module, where: i.id in ^(ids |> MapSet.new |> MapSet.to_list))])
   end
+
+  defp extract_references([ { _, { :ok, accounts } }, { _, { :ok, users } } ]), do: { accounts, users }
+  defp extract_references([ { _, { :error, _ } }    , { _, { :ok, users } } ]), do: { [], users }
+  defp extract_references([ { _, { :ok, accounts } }, { _, { :error, _ } } ]) , do: { accounts, [] }
+  defp extract_references([ { _, { :error, _ } }    , { _, { :error, _ } } ]) , do: { [], [] }
 end
