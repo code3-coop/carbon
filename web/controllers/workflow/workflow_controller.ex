c5a5921cb3d316d1f66cce0b4bb2b5af18323225
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-09-06 16:52:01 -0400

    add creation of workflow

--- web/controllers/workflow/workflow_controller.ex
+++ web/controllers/workflow/workflow_controller.ex
@@ -2,6 +2,28 @@ defmodule Carbon.Workflow.WorkflowController do
   use Carbon.Web, :controller
   alias Carbon.{ Account, User, Workflow }
 
+  def new(conn, _params) do
+    workflow = %Workflow{}
+    changeset = Workflow.changeset(workflow)
+    conn
+      |> assign(:changeset, changeset)
+      |> render("new.html")
+  end
+
+  def create(conn, %{"workflow" => workflow_params}) do
+    changeset = Workflow.changeset(%Workflow{}, workflow_params)
+    case Repo.insert(changeset) do
+      {:ok, workflow} ->
+        conn
+        |> put_flash(:info, "Workflow was successfully created")
+        |> redirect(to: workflow_path(conn, :edit, workflow.id))
+      {:error, changeset} ->
+        conn
+        |> assign(:changeset, changeset)
+        |> render("new.html")
+    end
+  end
+
   def index(conn, _params) do
     query = from w in Workflow,
       where: w.active
