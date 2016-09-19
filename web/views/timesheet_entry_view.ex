23adf9c8c94871f4806d3edb63dace20984d6953
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-09-19 16:00:54 -0400

    split acount and project fields in timesheet entry new

--- web/views/timesheet_entry_view.ex
+++ web/views/timesheet_entry_view.ex
@@ -1,14 +1,16 @@
 defmodule Carbon.TimesheetEntryView do
   use Carbon.Web, :view
   import Carbon.ViewHelpers, only: [timehseet_entry_tags_select: 0]
-  import Ecto.Query, only: [from: 2]
+  import Ecto.Query, only: [from: 1]
 
 
   def project_select do
-    query = from p in Carbon.Project,
-      order_by: p.id,
-      left_join: a in assoc(p, :account),
-      preload: [account: a]
+    query = from p in Carbon.Project
+    Carbon.Repo.all(query)
+  end
+
+  def account_select do
+    query = from a in Carbon.Account
     Carbon.Repo.all(query)
   end
 
