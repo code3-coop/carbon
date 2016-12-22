6114aa5dfe1b52a3da7ca534f8068afc8e1a9dc0
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-12-22 13:13:57 -0500

    add a task to run migration on release

---
+++ lib/carbon/release_tasks.ex
@@ -0,0 +1,11 @@
+defmodule Release.Tasks do
+  def migrate do
+    {:ok, _} = Application.ensure_all_started(:carbon)
+
+    path = Application.app_dir(:carbon, "priv/repo/migrations")
+
+    Ecto.Migrator.run(Carbon.Repo, path, :up, all: true)
+
+    :init.stop()
+  end
+end
