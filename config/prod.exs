fe82615e05e00b3c7e9a2b638434d96a099f4343
Benjamin Dreux <benjamin.dreux@code3.ca>
2016-12-22 13:56:27 -0500

    change server port

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :carbon, Carbon.Endpoint,
   http: [port: 4000],
-  url: [host: "carbon.code3.ca"],
+  url: [host: "carbon.code3.ca", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
