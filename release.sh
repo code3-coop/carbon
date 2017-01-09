f242d6a9933a005a8c875be9db3dd0ece17407cc
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-09 12:19:34 -0500

    fix migration

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ pwd
 _build/prod/rel/carbon/bin/carbon stop
 git checkout --force
 mix deps.get
-_build/prod/rel/carbon/bin/carbon Release.Tasks migrate
+_build/prod/rel/carbon/bin/carbon command Release.Tasks migrate
 MIX_ENV=prod mix release --env=prod
 _build/prod/rel/carbon/bin/carbon start
