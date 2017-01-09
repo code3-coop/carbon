8256267e4d720a2763decd61c487c09ab4feea28
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-09 11:40:12 -0500

    Fix fun path

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@ cd /home/sshadmin/carbon
 _build/prod/rel/carbon/bin/carbon stop
 git checkout --force
 mix deps.get
-_build/prod/rel/carbon/bin/carbon Elixir.Release.Tasks migrate
+_build/prod/rel/carbon/bin/carbon Release.Tasks migrate
 MIX_ENV=prod mix release --env=prod
 _build/prod/rel/carbon/bin/carbon start
