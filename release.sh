9c9a76e3b44c379107fb80dbea9aa45da7870f13
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-09 11:17:56 -0500

    force pull of update

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 _build/prod/rel/carbon/bin/carbon stop
+git checkout --force
 mix deps.get
-# rel/my_app/bin/carbon Elixir.Release.Tasks migrate
+rel/my_app/bin/carbon Elixir.Release.Tasks migrate
 MIX_ENV=prod mix release --env=prod
 _build/prod/rel/carbon/bin/carbon start
