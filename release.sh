00d98d65f5dfb724a3977d5618ff2f6e76ea2f87
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-09 11:28:32 -0500

    Fix bd migration on release

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@ cd /home/sshadmin/carbon
 _build/prod/rel/carbon/bin/carbon stop
 git checkout --force
 mix deps.get
-rel/my_app/bin/carbon Elixir.Release.Tasks migrate
+carbon/_build/prod/rel/carbon/bin/carbon Elixir.Release.Tasks migrate
 MIX_ENV=prod mix release --env=prod
 _build/prod/rel/carbon/bin/carbon start
