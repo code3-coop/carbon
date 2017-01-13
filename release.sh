24e604d8ad02e7f2e90744cc3aec119f331c1fd4
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-13 13:52:03 -0500

    ensure everthing is ok before running the release

--- release.sh
+++ release.sh
@@ -1,22 +1,16 @@
 echo Running release
 
-cd /home/sshadmin/carbon
-GIT_DIR=/home/sshadmin/carbon/.git
-
 echo Stopping current instance
 _build/prod/rel/carbon/bin/carbon stop
 
-echo Pulling last changes
-git --work-tree=${pwd} checkout --force
-
-echo Updating dependcies
+echo Updating dependencies
 mix deps.get
-
-echo Running migrations
-_build/prod/rel/carbon/bin/carbon command Carbon.Release.Tasks migrate
 
 echo Building release
 MIX_ENV=prod mix release --env=prod
+
+echo Running migrations
+_build/prod/rel/carbon/bin/carbon command Elixir.Carbon.Release.Tasks migrate
 
 echo Starting app
 _build/prod/rel/carbon/bin/carbon start
