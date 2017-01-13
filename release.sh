d59cdd48b33836d58ae96928727db8ea3adee110
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-13 12:05:59 -0500

    Let's do that shit

--- release.sh
+++ release.sh
@@ -2,11 +2,22 @@ echo Running release
 
 cd /home/sshadmin/carbon
 GIT_DIR=/home/sshadmin/carbon/.git
-echo GIT_DIR $GIT_DIR
 
+
+echo Stopping current instance
 _build/prod/rel/carbon/bin/carbon stop
+
+echo Pulling last changes
 git --work-tree=${pwd} checkout --force
+
+echo Updating dependcies
 mix deps.get
+
+echo Running migrations
 _build/prod/rel/carbon/bin/carbon command Release.Tasks migrate
+
+echo Building release
 MIX_ENV=prod mix release --env=prod
+
+echo Starting app
 _build/prod/rel/carbon/bin/carbon start
