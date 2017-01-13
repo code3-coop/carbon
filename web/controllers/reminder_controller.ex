01d46dc167a476158d53b510ae7981abbfb8f7c5
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-13 09:49:12 -0500

    when setting a new reminder, if the date is invalid, the time is also shown as invalid

--- web/controllers/reminder_controller.ex
+++ web/controllers/reminder_controller.ex
@@ -12,20 +12,19 @@ defmodule Carbon.ReminderController do
     |> assign(:changeset, changeset)
     |> assign(:account_id, account_id)
     |> assign(:event_id, event_id)
-    |> render("new.html")  
+    |> render("new.html")
   end
 
   def create(conn, %{"account_id" => account_id, "event_id" => event_id, "reminder" => reminder_params}) do
     current_user = conn.assigns[:current_user]
     reminder = %Reminder{user: current_user, event: Repo.get(Event, String.to_integer(event_id))}
-    date = Ecto.DateTime.cast!("#{reminder_params["date"]} #{reminder_params["time"]}:00")
-    changeset = Reminder.create_changeset(reminder, %{date:  date})
+    changeset = Reminder.create_changeset(reminder, reminder_params)
     case Repo.insert(changeset) do
       {:ok, _reminder} ->
         conn
         |> put_flash(:info, "Reminder created successfully.")
         |> redirect(to: account_event_path(conn, :index, account_id))
-      {:error, changeset} -> 
+      {:error, changeset} ->
         conn
         |> put_flash(:info, "Reminder could not be created.")
         |> assign(:changeset, changeset)
@@ -35,12 +34,12 @@ defmodule Carbon.ReminderController do
     end
   end
 
-  def delete(conn, _params) do 
+  def delete(conn, _params) do
     %{:params => %{"account_id" => account_id, "id" => reminder_id}} = conn
     reminder = Repo.get(Reminder, reminder_id)
     changeset = Reminder.archive_changeset(reminder, %{active: false})
     case Repo.update(changeset) do
-      {:ok, reminder} -> 
+      {:ok, reminder} ->
         conn
         |> put_flash(:deleted_reminder, reminder)
         |> redirect(to: account_event_path(conn, :index, account_id))
@@ -51,12 +50,12 @@ defmodule Carbon.ReminderController do
         |> render(account_event_path(conn, :index, account_id))
     end
   end
-  def restore(conn, _params) do 
+  def restore(conn, _params) do
     %{:params => %{"account_id" => account_id, "id" => reminder_id}} = conn
     reminder = Repo.get(Reminder, reminder_id)
     changeset = Reminder.archive_changeset(reminder, %{active: true})
     case Repo.update(changeset) do
-      {:ok, _reminder} -> 
+      {:ok, _reminder} ->
         conn
         |> redirect(to: account_event_path(conn, :index, account_id))
       {:error, changeset} ->
--- web/models/reminder.ex
+++ web/models/reminder.ex
@@ -22,17 +22,38 @@ defmodule Carbon.Reminder do
     |> cast(params, [:date])
     |> validate_required([:date])
   end
-  
+
+
+  defp merge_date_time_to_datetime(%{"date" => date, "time" => time} = params) when date != "" and time != ""do
+    datetime = Ecto.DateTime.cast!("#{date} #{time}:00")
+    Map.put(params, "date", datetime)
+  end
+  defp merge_date_time_to_datetime(params) do
+    Map.delete(params, "date")
+  end
+
   def create_changeset(struct, params) do
     struct
-    |> cast(params, [:date])
+    |> cast(merge_date_time_to_datetime(params), [:date])
     |> validate_required([:date])
+    |> copyDateErrorToTimeError()
   end
+
+  defp copyDateErrorToTimeError(changeset) do
+    if Map.has_key?(changeset, :errors) and Keyword.has_key?(changeset.errors, :date) do
+      Map.update! changeset, :errors, fn current_errors ->
+        Keyword.put current_errors, :time, {"is invalid", [type: Ecto.Time]}
+      end
+    else
+      changeset
+    end
+  end
+
 
   def archive_changeset(struct, params) do
     struct
     |> cast(params, [:active])
     |> validate_required([:active])
   end
-  
+
 end
