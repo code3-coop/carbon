38f90687a582767b82d136802d594bebf03a457c
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-13 11:02:33 -0500

    remove disabled project/account from timesheet entry inputs

--- web/views/timesheet_entry_view.ex
+++ web/views/timesheet_entry_view.ex
@@ -1,16 +1,18 @@
 defmodule Carbon.TimesheetEntryView do
   use Carbon.Web, :view
   import Carbon.ViewHelpers, only: [timehseet_entry_tags_select: 0]
-  import Ecto.Query, only: [from: 1]
+  import Ecto.Query, only: [from: 2]
 
 
   def project_select do
-    query = from p in Carbon.Project
+    query = from p in Carbon.Project,
+      where: p.active == true
     Carbon.Repo.all(query)
   end
 
   def account_select do
-    query = from a in Carbon.Account
+    query = from a in Carbon.Account,
+      where: a.active == true
     Carbon.Repo.all(query)
   end
 
