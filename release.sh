4e3f8b28d1ecc3766f5f1cf3582338df093b0b23
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-01-13 11:49:34 -0500

    trying to checkout from fresh source at each pull

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ echo Running release
 cd /home/sshadmin/carbon
 pwd
 _build/prod/rel/carbon/bin/carbon stop
-git checkout --force
+git --work-tree=${pwd} checkout --force
 mix deps.get
 _build/prod/rel/carbon/bin/carbon command Release.Tasks migrate
 MIX_ENV=prod mix release --env=prod
